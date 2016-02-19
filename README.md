e25411cb77e0cae128c54e625e8512874f8a8001
Matt McShane <matt@mattmcshane.com>
2016-02-19 04:19:25 +0000

    Bi Ag Feitheamh

---
+++ README.md
@@ -0,0 +1,12 @@
+# Left-Right Concurrency Control
+
+This is an implementation of left-right concurrency control in C++. The
+concurrency control mechanism is by Pedro Ramalhete and Andreia Correia. My
+contribution is solely an implementation that makes use of modern C++ features.
+
+As I write this, papers on this concurrency control technique are forthcoming,
+for the time being the best reference is probably
+http://concurrencyfreaks.blogspot.com/2013/12/left-right-concurrency-control.html
+
+This is a header-only library. Simply put the leftright.hpp file somewhere in
+that your C++ compiler can find it and you're good to go.
---
+++ test/test_leftright.cpp
@@ -0,0 +1,93 @@
+#include <mpm/leftright.hpp>
+#include <map>
+#include <string>
+#include <catch.hpp>
+
+
+TEST_CASE("in-place construct", "[mpm.leftright]")
+{
+    using lrstring = mpm::leftright<std::string>;
+
+    lrstring lrs(mpm::in_place, 3, '*');
+    lrs.observe([](lrstring::const_reference str) {
+        CHECK(str == "***");
+    });
+}
+
+
+TEST_CASE("modify read modify read", "[mpm.leftright]")
+{
+    using lrmap = mpm::leftright<std::map<int, int>>;
+    lrmap lrm;
+    lrm.modify([](lrmap::reference map) noexcept {
+        map[1] = 1;
+    });
+
+    int value = lrm.observe([](lrmap::const_reference map) { return map.find(1)->second; });
+    CHECK(1 == value);
+
+
+    lrm.modify([](lrmap::reference map) noexcept {
+        map[2] = 2;
+    });
+
+    value = lrm.observe([](lrmap::const_reference map) { return map.find(2)->second; });
+    CHECK(2 == value);
+}
+
+
+TEST_CASE("distributed modify read modify read", "[mpm.leftright]")
+{
+    using lrmap = mpm::basic_leftright<std::map<int, int>,
+          mpm::distributed_atomic_reader_registry<4>>;
+    lrmap lrm;
+    lrm.modify([](lrmap::reference map) noexcept {
+        map[1] = 1;
+    });
+
+    int value = lrm.observe([](lrmap::const_reference map) { return map.find(1)->second; });
+    CHECK(1 == value);
+
+
+    lrm.modify([](lrmap::reference map) noexcept {
+        map[2] = 2;
+    });
+
+    value = lrm.observe([](lrmap::const_reference map) { return map.find(2)->second; });
+    CHECK(2 == value);
+}
+
+
+TEST_CASE("noexcept specs", "[mpm.leftright]")
+{
+    using lrint = mpm::leftright<int>;
+    lrint lri{0};
+
+    auto null_observer = [](lrint::const_reference i){ return i; };
+    auto noexcept_null_observer = [](lrint::const_reference i) noexcept { return i; };
+
+    CHECK(noexcept(lri.observe(noexcept_null_observer)));
+    CHECK(!noexcept(lri.observe(null_observer)));
+
+    auto noexcept_null_modifier = [](lrint::reference i) noexcept { return i; };
+
+    CHECK(0 == lri.modify(noexcept_null_modifier));
+
+    // this doesn't and shouldn't compile because modify only exists for
+    // functors that are declared noexcept
+    //auto null_modifier = [](lrint::reference i){ return i; };
+    //CHECK(0 == lri.modify(null_modifier));
+}
+
+/*
+struct unsafe_reader_registry
+{
+    void arrive(){}; //missing required noexcept
+    void depart(){}; //missing required noexcept
+    bool empty() const { return true; } //missing required noexcept
+};
+
+// This shouldn't and doesn't compile because the functions on
+// unsafe_reader_registry are not noexcept
+template class mpm::basic_leftright<int, unsafe_reader_registry>;
+*/
