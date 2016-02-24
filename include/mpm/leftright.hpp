1257e84ba306d7e73274d6d6a20621f00f7e06b1
Matt McShane <matt@mattmcshane.com>
2016-02-24 02:38:44 +0000

    Inline definition of in_place_t

--- include/mpm/leftright.hpp
+++ include/mpm/leftright.hpp
@@ -42,9 +42,7 @@ namespace mpm
     //! | r.empty()  | const and noexcept.    | true if there are no readers; false otherwise. |
     //! \}
 
-    struct in_place_t {};
-    constexpr in_place_t in_place{};
-
+    constexpr struct in_place_t { } in_place { };
 
     //! Wrap any single-threaded datastructure with Left-Right
     //! concurrency control
